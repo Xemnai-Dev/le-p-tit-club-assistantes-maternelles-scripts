5eb220073125d4f10092d7dbf29f85ae79f24619
Xemnai-dev <xemnaidev@gmail.com>
2021-01-19 20:28:15 +0100

    Create Maine-et-Loire.py
    Add script

---
+++ Maine-et-Loire.py
@@ -0,0 +1,50 @@
+# import libraries
+from bs4 import BeautifulSoup
+import urllib.request
+import csv
+
+#open file
+fichier = open("Maine-et-Loire.csv","a")
+fichier.write('Nom Prenom;Adresse;Code postale Ville;\n')
+
+
+for i in range(5):
+    print('Page ' + str(i) + ' done')
+    # specify the url
+    urlpage = 'https://www.assistantsmaternels49.fr/index.php?id=1577&page_directe=' + str(i+1)
+
+    # query the website and return the html to the variable 'page'
+    page = urllib.request.urlopen(urlpage)
+    # parse the html using beautiful soup and store in variable 'soup'
+    soup = BeautifulSoup(page, 'html.parser')
+
+    strsoup = str(soup)
+    index = strsoup.find('<!-- LIGNES DE RESULTATS -->')+28+211+3
+    strsoup = strsoup[index:]
+    index = strsoup.find('<')
+    
+    if i == 109:
+        rangee = 15
+    else:
+        rangee = 50
+            
+    for y in range(rangee):
+        index = strsoup.find('Voir la fiche de')+16
+        strsoup =strsoup[index:]
+        index = strsoup.find('"')
+        nom_prenom = (strsoup[1:index]) 
+        strsoup = strsoup[index:]
+        index = strsoup.find('</td><td>')+9
+        strsoup = strsoup[index:]
+        index = strsoup.find('Voir la fiche</a>') + 21
+        strsoup = strsoup[index:]
+        index = strsoup.find(', 49')
+        adresse = (strsoup[:index]) 
+        strsoup = strsoup[index+2:]
+        index = strsoup.find('MAINE ET LOIRE')-1
+        cp_ville = (strsoup[:index]) 
+        fichier.write(nom_prenom+';'+adresse+';'+cp_ville+';\n')
+
+                
+
+fichier.close()
